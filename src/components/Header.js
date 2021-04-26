0588e12475b5447f2403480e8eaf1a68f86f0871
platzh1rsch <chregi.glatthard@gmail.com>
2021-04-26 22:59:51 +0200

    good night

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,23 @@ import styled from 'styled-components/macro'
 import Contact from './molecules/Contact'
 
 const StyledHeader = styled.h1`
-  font-family: 'Nineteen Ten Vienna', sans-serif
+  font-family: 'Nineteen Ten Vienna', sans-serif;
+  margin-bottom: 0;
 `
+
+const StyledNavLink = styled(NavLink)`
+  margin: 0;
+`;
 
 export default () => (
   <header className='Header-header'>
-    <StyledHeader className='Header-h1'>Yes it's Ananias</StyledHeader>
+    <StyledNavLink
+        exact
+        to='/'
+      >
+        <StyledHeader className='Header-h1'>Yes it's Ananias</StyledHeader>
+      </StyledNavLink>
+    
     <h2 className='Header-h2'>Contemporary Psycho-Automatic Piano</h2>
 
     <Contact />
