6abb89480b01729f4629a855fd6209a9fffff171
platzh1rsch <chregi.glatthard@gmail.com>
2021-08-30 20:02:36 +0200

    small rearrangement

--- src/components/molecules/EventListItem.js
+++ src/components/molecules/EventListItem.js
@@ -15,6 +15,12 @@ const StyledAlbumDescription = styled.div`
     flex: 1 1 auto;
 `
 
+const EventTimeAndLocation = styled.p`
+  span:nth-child(2n) {
+    margin-left: 24px;
+  }
+`;
+
 export default props => {
   const { event } = props
   const date = event.date ? new Date(event.date) : undefined;
@@ -22,13 +28,14 @@ export default props => {
   const formattedDate = dateTime ? `${dateTime.toLocaleString({ weekday: 'long', month: 'long', day: '2-digit', year: 'numeric' })}` : '';
 
   return <StyledEventListItem key={event.id}>
-        <StyledAlbumDescription>
-      <h2>{event.title}</h2>
+    <StyledAlbumDescription>
       <p>{formattedDate}</p>
-      {event.venueUrl ? <p><a href={event.venueUrl}>{event.venueName}</a></p> : <p>{event.venueName}</p>}
-      <p>{event.time}</p>
-
-      {event.eventUrl && <p><a href={event.eventUrl}>{event.eventUrlText ?? 'Event Details'}</a></p>}
+      <h2>{event.title}</h2>
+      <EventTimeAndLocation>
+        {event.venueUrl ? <span title="Location"><i className="fas fa-map-marker-alt"></i> <a href={event.venueUrl} rel="noopener noreferrer" target="_blank">{event.venueName}</a></span> : <span title="Location"><i className="fas fa-map-marker-alt"></i> {event.venueName}</span>}
+        <span title="Show start"><i className="far fa-clock"></i> {event.time}</span>
+      </EventTimeAndLocation>
+      {event.eventUrl && <p title="More information"><i className="fas fa-external-link-alt"></i> <a href={event.eventUrl} rel="noopener noreferrer" target="_blank">{event.eventUrlText ?? 'Event Details'}</a></p>}
     </StyledAlbumDescription>
   </StyledEventListItem>
 }
