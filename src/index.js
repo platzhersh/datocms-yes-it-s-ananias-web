1e4f6423a73ec0ac355315c12b2dd5a3b11f7fc9
platzh1rsch <chregi.glatthard@gmail.com>
2021-10-27 19:35:04 +0200

    add google tag manager

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-import './fonts.scss'
-import './old-style.scss'
-import './index.scss'
-import App from './components/App'
 import { ApolloProvider } from 'react-apollo'
+import ReactDOM from 'react-dom'
+import TagManager from 'react-gtm-module'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import client from './client'
+import App from './components/App'
+import './fonts.scss'
+import './index.scss'
+import './old-style.scss'
+
+const tagManagerArgs = {
+  gtmId: process.env.GTM_ID ?? undefined,
+}
+
+if (tagManagerArgs.gtmId) {
+  TagManager.initialize(tagManagerArgs)
+}
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
