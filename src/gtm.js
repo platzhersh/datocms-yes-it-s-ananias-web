7519ec37a9664ebf36167874b74c01f2323618a3
platzh1rsch <chregi.glatthard@gmail.com>
2021-11-11 00:20:41 +0100

    seems good now

--- src/gtm.js
+++ src/gtm.js
@@ -4,7 +4,6 @@ import TagManager from 'react-gtm-module'
  * Google Tag Manager Init Script
  */
 export default function initGtm() {
-  console.log(process.env)
   const tagManagerArgs = {
     gtmId: process.env.REACT_APP_GTM_ID ?? undefined,
   }
