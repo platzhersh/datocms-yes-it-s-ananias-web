447940d5ff14bf7216f21e36d9e749ae46bf7184
platzh1rsch <chregi.glatthard@gmail.com>
2024-02-11 13:00:36 +0100

    chore: adjust error message

--- src/components/atoms/ErrorMessage/ErrorMessage.tsx
+++ src/components/atoms/ErrorMessage/ErrorMessage.tsx
@@ -25,16 +25,15 @@ export const ErrorMessage = (props: ErrorMessageProps) => {
   const { error } = props
   const errorMessage = error?.message ?? ''
   console.error(errorMessage)
-  const noInternetConnection = errorMessage.includes('Network error')
+  const noInternetConnection = errorMessage.includes('NetworkError')
+  const authenticationError = errorMessage.includes('401')
   return (
     <StyledItemContainer>
       <div>
-        <StyledHeader>OOPS-YIA</StyledHeader>
-        {!noInternetConnection && (
-          <p>Something went wrong loading this content...</p>
-        )}
-        {noInternetConnection && (
-          <p>Looks like you are not connected to the internet...</p>
+        <StyledHeader>OOPS-YIA</StyledHeader>    
+        <p>Something went wrong loading this content...</p>
+        {authenticationError && (
+          <p>Could not authenticate with the server.</p>
         )}
         {noInternetConnection && (
           <p>Check your internet connection and try to reload.</p>
--- src/components/features/Videos.jsx
+++ src/components/features/Videos.jsx
@@ -1,10 +1,10 @@
-import gql from 'graphql-tag'
-import React from 'react'
-import { Query } from 'react-apollo'
-import { VideoFragment } from '../../queries/fragments/VideoFragment'
-import { ErrorMessage } from '../atoms/ErrorMessage/ErrorMessage'
-import { LoadingPlaceholder } from '../atoms/LoadingPlaceholder/LoadingPlaceholder'
-import { VideoCard } from '../molecules/VideoCard/VideoCard'
+import gql from "graphql-tag";
+import React from "react";
+import { Query } from "react-apollo";
+import { VideoFragment } from "../../queries/fragments/VideoFragment";
+import { ErrorMessage } from "../atoms/ErrorMessage/ErrorMessage";
+import { LoadingPlaceholder } from "../atoms/LoadingPlaceholder/LoadingPlaceholder";
+import { VideoCard } from "../molecules/VideoCard/VideoCard";
 
 const videosQuery = gql`
   query VideosQuery {
@@ -12,27 +12,29 @@ const videosQuery = gql`
      ${VideoFragment}
     }
   }
-`
+`;
 
 const Videos = (props) => {
   return (
     <Query query={videosQuery}>
       {({ data, loading, error }) => {
-        if (loading) return <LoadingPlaceholder />
-        if (error) return <ErrorMessage error={error} />
+        if (loading) return <LoadingPlaceholder />;
+        if (error) return <ErrorMessage error={error} />;
 
         return (
-          <section>
-            <div>
-              {data.allVideos.map((video) => (
-                <VideoCard video={video} />
-              ))}
-            </div>
-          </section>
-        )
+          <>
+            <section>
+              <div>
+                {data.allVideos.map((video) => (
+                  <VideoCard video={video} />
+                ))}
+              </div>
+            </section>
+          </>
+        );
       }}
     </Query>
-  )
-}
+  );
+};
 
-export default Videos
+export default Videos;
